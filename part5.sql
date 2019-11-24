92059119352b12e80b2e7c57863122b7449d1e8b
hiwotberhane <hiwot.berhane28@gmail.com>
2019-11-24 08:55:17 +0500

    Created and executed all queries of part6 written by Hiwot berhane

--- part5.sql
+++ part5.sql
@@ -32,3 +32,30 @@ JOIN order_items as oi ON o.order_id = oi.order_id
 WHERE item_price > 400
 GROUP BY email_address
 ORDER BY order_total DESC;
+
+SELECT product_name,  SUM((item_price - discount_amount) * quantity) AS total_product
+FROM products as p
+JOIN order_items as oi ON p.product_id = oi.product_id
+GROUP BY product_name WITH ROLLUP;
+
+SELECT email_address, COUNT(DISTINCT oi.product_id) AS count_product
+FROM customers as c
+JOIN orders as o ON c.customer_id = o.customer_id
+JOIN order_items as oi ON o.order_id = oi.order_id
+GROUP BY email_address
+HAVING COUNT(DISTINCT oi.product_id) > 1
+ORDER BY email_address ASC;
+
+SELECT category_name, product_name, COUNT(quantity) as total_count
+FROM  products as p 
+JOIN order_items as oi ON p.product_id = oi.product_id
+JOIN categories as c ON c.category_id = p.category_id 
+WHERE category_name IS NOT NULL
+OR product_name  IS NOT NULL
+GROUP BY product_name WITH ROLLUP;
+
+SELECT order_id,  SUM((item_price - discount_amount) * quantity) as totalamount, SUM(Quantity) OVER (ORDER BY order_id ASEC)as total_amount_of_orders 
+FROM order_items 
+GROUP BY order_id;
+
+
