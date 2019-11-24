92436ce2bb47d8d567f7a629a016827db55c3ed2
kingsleydurich <chidex99022@gmail.com>
2019-11-24 06:31:00 +0500

    completed scripts for part 4

--- part4.sql
+++ part4.sql
@@ -14,3 +14,12 @@ discount_percent, date_added) VALUES (4, 'dgx_640','Yamaha DGX 640 88-Key Digita
 
 UPDATE products SET discount_percent = '35' WHERE product_id = 11;
 
+SELECT * FROM categories;
+DELETE FROM products where category_id = 4;
+ DELETE FROM categories where category_id = 4;
+ 
+ INSERT INTO customers (email_address, password, first_name, last_name) VALUES ('rick@raven.com', '', 'Rick', 'Raven');
+
+UPDATE customers SET password = 'secret' WHERE email_address = 'rick@raven.com';
+
+UPDATE customers SET password = 'reset';
