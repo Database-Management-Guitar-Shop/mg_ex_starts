c62f6749840d04ac1a4bbb011d23afe60ecd39ea
kingsleydurich <chidex99022@gmail.com>
2019-11-24 07:00:40 +0500

    completed queries for part 5

--- part5.sql
+++ part5.sql
@@ -8,3 +8,27 @@ ON c.category_id= p.category_id
 GROUP BY category_name
 ORDER BY count_of_product DESC;
 
+SELECT  SUM(item_price * quantity) AS total_prices, email_address, 
+SUM(discount_amount * quantity) AS total_discounted
+FROM Customers c
+JOIN orders as o ON c.customer_id = o.customer_id
+JOIN order_items as oi ON o.order_id = oi.order_id
+GROUP BY email_address
+ORDER BY total_prices DESC;
+
+SELECT COUNT(o.order_id) AS order_count,
+SUM((item_price - discount_amount) * quantity) AS order_total, email_address
+FROM customers as c
+JOIN orders as o ON c.customer_id = o.customer_id
+JOIN order_items as oi ON o.order_id = oi.order_id
+GROUP BY email_address
+ORDER BY order_total DESC;
+
+SELECT COUNT(o.order_id) AS order_count,
+SUM((item_price - discount_amount) * quantity) AS order_total, email_address
+FROM customers as c
+JOIN orders as o ON c.customer_id = o.customer_id
+JOIN order_items as oi ON o.order_id = oi.order_id
+WHERE item_price > 400
+GROUP BY email_address
+ORDER BY order_total DESC;
